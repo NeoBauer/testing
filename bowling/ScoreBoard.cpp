847b43589210af473ca5062ee62a6e28ca232798
Kuba C <jakub.cieplinski@outlook.com>
2020-08-18 14:57:15 +0200

    beggining of scores extract

--- bowling/ScoreBoard.cpp
+++ bowling/ScoreBoard.cpp
@@ -1,5 +1,20 @@
 #include "ScoreBoard.hpp"
 
+using mapOfScoresWithNames = std::map<std::string, std::vector<std::pair<std::string, size_t>>>;
+
 mapOfScoresWithNames ScoreBoard::calculateScores(std::map<std::string, std::vector<std::string>> fileContent) {
-    return {};
+    mapOfScoresWithNames returnMap{};
+    std::string nameDelimeter = ":";
+    std::string scoreDelimeter = "|";
+
+    for (auto& element : fileContent) {
+        std::pair<std::string, std::vector<std::pair<std::string, size_t>>> para;
+        para.first = element.first;
+        for (auto& vectorEl : element.second) {
+            std::pair<std::string, size_t> scoreToInsert{};
+            scoreToInsert.first = vectorEl.substr(0, vectorEl.find(nameDelimeter));
+        }
+
+        returnMap.insert(para);
+    }
 }
