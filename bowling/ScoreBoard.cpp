c46618859e4257fb884ee8b0ef5b2ac28de2e8a9
Kuba C <jakub.cieplinski@outlook.com>
2020-08-30 13:00:29 +0200

    Implemented extracting and assigning players strikes to their names and players themselves to lanes

--- bowling/ScoreBoard.cpp
+++ bowling/ScoreBoard.cpp
@@ -1,20 +1,41 @@
 #include "ScoreBoard.hpp"
+#include <algorithm>
 
-using mapOfScoresWithNames = std::map<std::string, std::vector<std::pair<std::string, size_t>>>;
+using mapOfScoresWithNames = std::map<std::string, std::vector<std::pair<std::string, size_t>>>;  //std::map<laneName, std::vector<std::pair<playerName, theirScore>>>
+using pairOfScoreSymbolsForPlayer = std::pair<std::string, std::vector<std::string>>;             // std::pair<playerName, theirStrikeSybols>
 
-mapOfScoresWithNames ScoreBoard::calculateScores(std::map<std::string, std::vector<std::string>> fileContent) {
-    mapOfScoresWithNames returnMap{};
-    std::string nameDelimeter = ":";
-    std::string scoreDelimeter = "|";
+pairOfScoreSymbolsForPlayer ScoreBoard::extractScoresForPlayer(std::string& fileContent) {
+    pairOfScoreSymbolsForPlayer playerScores{};
+    std::string scoresString{};
+    int delimeterCount{};
 
-    for (auto& element : fileContent) {
-        std::pair<std::string, std::vector<std::pair<std::string, size_t>>> para;
-        para.first = element.first;
-        for (auto& vectorEl : element.second) {
-            std::pair<std::string, size_t> scoreToInsert{};
-            scoreToInsert.first = vectorEl.substr(0, vectorEl.find(nameDelimeter));
+    std::string nameDelimeter{":"};
+    char scoreDelimeter = '|';
+
+    playerScores.first = fileContent.substr(0, fileContent.find(nameDelimeter));
+    scoresString = fileContent.substr(fileContent.find(nameDelimeter) + 1, fileContent.length());
+
+    delimeterCount = std::count_if(scoresString.begin(), scoresString.end(), [&scoreDelimeter](auto& el) { return el == scoreDelimeter; });  // if needed for points count, can be removed
+    scoresString.erase(std::remove(scoresString.begin(), scoresString.end(), scoreDelimeter), scoresString.end());
+
+    std::for_each(scoresString.begin(), scoresString.end(), [&playerScores](auto el) { playerScores.second.push_back(std::string(1, el)); });
+
+    return playerScores;
+}
+
+std::map<std::string, std::vector<pairOfScoreSymbolsForPlayer>> ScoreBoard::pairPlayersWithLanes(std::map<std::string, std::vector<std::string>>& fileContent) {
+    std::map<std::string, std::vector<pairOfScoreSymbolsForPlayer>> lanesMap;  //std::map<laneName, std::vector<std::pair<playerName, theirStrikeSybols>>>
+
+    for (auto& fileElement : fileContent) {
+        std::vector<pairOfScoreSymbolsForPlayer> players{};
+        for (auto& vectorElement : fileElement.second) {
+            players.push_back(extractScoresForPlayer(vectorElement));
         }
-
-        returnMap.insert(para);
+        lanesMap[fileElement.first] = players;
     }
+
+    return lanesMap;
+}
+
+mapOfScoresWithNames ScoreBoard::calculateScores(std::map<std::string, std::vector<pairOfScoreSymbolsForPlayer>>& lanes) {
 }
--- bowling/ScoreBoard.hpp
+++ bowling/ScoreBoard.hpp
@@ -3,8 +3,12 @@
 #include <vector>
 
 using mapOfScoresWithNames = std::map<std::string, std::vector<std::pair<std::string, size_t>>>;
+using pairOfScoreSymbolsForPlayer = std::pair<std::string, std::vector<std::string>>;
 
 class ScoreBoard {
+private:
+    std::map<std::string, std::vector<pairOfScoreSymbolsForPlayer>> pairPlayersWithLanes(std::map<std::string, std::vector<std::string>>& fileContent);
+    pairOfScoreSymbolsForPlayer extractScoresForPlayer(std::string& playerScores);
 public:
-    mapOfScoresWithNames calculateScores(std::map<std::string, std::vector<std::string>> fileContent);
+    mapOfScoresWithNames calculateScores(std::map<std::string, std::vector<pairOfScoreSymbolsForPlayer>>& playersWithLanes);
 };
